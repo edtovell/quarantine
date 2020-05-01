307fcdfce0286ace2c34532caa4c9023f5637ad6
edatpb <ed@playbrush.io>
2020-05-01 21:14:37 +0100

    jogger NPCs move, but no collision yet

--- src/exercise.js
+++ src/exercise.js
@@ -19,6 +19,7 @@ class Exercise extends Phaser.Scene {
         })
 
         this.load.spritesheet("pc_e", "./assets/pc/pc_exercise_spritesheet.png", { frameWidth: 47, frameHeight: 74, spacing: 2, });
+        this.load.spritesheet("joggerA", "./assets/npcs/jogger_A_spritesheet.png", { frameWidth: 49, frameHeight: 99, spacing: 2, });
         this.load.image("park", "./assets/tiles/park.png");
 
         this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -56,7 +57,7 @@ class Exercise extends Phaser.Scene {
             startAt: 250,
         });
 
-        pc.setDepth(1);
+        pc.setDepth(2);
         this.pc = pc;
 
         // Player has an aura representing 6ft radius
@@ -64,7 +65,7 @@ class Exercise extends Phaser.Scene {
         aura.fillStyle("0x00BFFF");
         aura.fillCircle(pc.x, pc.y, 200);
         aura.setAlpha(0.2);
-        aura.setDepth(0);
+        aura.setDepth(1);
 
         this.tweens.add({
             targets: aura,
@@ -75,15 +76,15 @@ class Exercise extends Phaser.Scene {
         });
         this.aura = aura;
 
-        // Tween constantly running, moving player to target
+        // Tween constantly moving player to whichever track he's in
         pc.trackIndex = 2 // i.e. the middle track out of 5
         pc.moveDistance = 160 // how far player moves between tracks
         this.trackXCoords = new Array(
-            this.cam.midPoint.x - (pc.moveDistance*2),
+            this.cam.midPoint.x - (pc.moveDistance * 2),
             this.cam.midPoint.x - pc.moveDistance,
             this.cam.midPoint.x,
             this.cam.midPoint.x + pc.moveDistance,
-            this.cam.midPoint.x + (pc.moveDistance*2),
+            this.cam.midPoint.x + (pc.moveDistance * 2),
         );
         pc.trackTween = this.tweens.add({
             targets: pc,
@@ -91,30 +92,78 @@ class Exercise extends Phaser.Scene {
             x: this.trackXCoords[pc.trackIndex],
             duration: 300,
         });
+
+        // Spawn joggers
+        this.nJoggers = 0;
+        this.joggers = new Array();
+        this.time.addEvent({
+            args: [2],
+            callback: this.spawnJoggers,
+            callbackScope: this,
+            delay: 5000,
+            loop: true,
+        });
     }
 
     update() {
         var pc = this.pc;
-        if (Phaser.Input.Keyboard.JustDown(this.leftKey)){
+        if (Phaser.Input.Keyboard.JustDown(this.leftKey) && pc.trackIndex > 0) {
             pc.trackIndex--;
             pc.trackTween.restart();
         }
-        if (Phaser.Input.Keyboard.JustDown(this.rightKey)){
+        if (Phaser.Input.Keyboard.JustDown(this.rightKey) && pc.trackIndex < 4) {
             pc.trackIndex++;
             pc.trackTween.restart();
-        }
-
-        if (pc.trackIndex < 0){
-            pc.trackIndex = 0;
-        } else if (pc.trackIndex > 4){
-            pc.trackIndex = 4;
         }
 
         pc.trackTween.updateTo("x", pc.x, true); // 3rd arg updates the start value
         pc.trackTween.updateTo("x", this.trackXCoords[pc.trackIndex]);
 
-
-
         this.aura.setX(pc.x - 400);
+
+        // If you hit a jogger, u ded
+        this.joggers.forEach(function(jogger) {
+            var scene = jogger.scene;
+            if (scene.physics.overlap(jogger, scene.aura)) {
+                scene.aura.setTint("0xDC143C");
+                scene.scene.pause("exercise");
+            }
+        });
+    }
+
+    spawnJoggers(n) {
+        var modelPool = ["joggerA"];
+        for (let i = n; i > 0; i--) {
+            var model = Phaser.Math.RND.pick(modelPool);
+            var jogger = this.physics.add.sprite(this.cam.midPoint.x, -200, model, 0);
+            jogger.setScale(2.5);
+            this.joggers.push(jogger);
+
+            // run animation
+            var animName = "npc_run_" + this.nJoggers;
+            this.nJoggers++;
+            this.anims.create({
+                key: animName,
+                frames: this.anims.generateFrameNumbers(model, { frames: [0, 1] }),
+                frameRate: 2,
+                repeat: -1,
+            });
+            jogger.anims.play(animName);
+            var targetX = Phaser.Math.RND.pick(this.trackXCoords);
+
+            // move down screen
+            this.tweens.add({
+                targets: jogger,
+                x: targetX,
+                y: 800,
+                duration: 5000,
+                onComplete: () => {
+                    var scene = jogger.scene;
+                    var index = scene.joggers.indexOf(jogger);
+                    scene.joggers.splice(index, 1);
+                    jogger.destroy();
+                }
+            });
+        }
     }
 }
